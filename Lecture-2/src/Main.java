bd187cbecdf85794b90928f3293dbbf52c5b69d0
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-02-21 19:52:38 -0500

    Lec -2

--- Lecture-2/src/Main.java
+++ Lecture-2/src/Main.java
@@ -55,6 +55,11 @@ public class Main {
         main.myTest();
         main.firstName= "John";
         System.out.println(main.firstName);
+        main.squarePattern();
+        System.out.println("################################");
+        main.diamondPattern();
+        System.out.println("################################");
+        main.butterflyPattern();
     }
 
     public static void myVoidMethod(int i) {
@@ -72,5 +77,83 @@ public class Main {
     private void myTest() {
         System.out.println("Test");
     }
+
+    private void squarePattern() {
+        for (int i = 0; i  <  5; i++) {
+            for (int j = 0; j  <  5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private void diamondPattern() {
+        int n = 5;
+
+        // Upper half of the diamond
+        for (int i = 0; i  <  n; i++) {
+            // Print spaces
+            for (int j = n - 1; j  >  i; j--) {
+                System.out.print(" ");
+            }
+            // Print asterisks
+            for (int k = 0; k  <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = 1; i  <  n; i++) {
+            // Print spaces
+            for (int j = 0; j  <  i; j++) {
+                System.out.print(" ");
+            }
+            // Print asterisks
+            for (int k = n - 1; k  >= i; k--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void butterflyPattern() {
+        int n = 5;
+
+        // Upper half of the diamond
+        for (int i = 1; i  <= n; i++) {
+            // Print left half of the row
+            for (int j = 1; j  <= i; j++) {
+                System.out.print("*");
+            }
+            // Print spaces in the middle
+            for (int j = 1; j  <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Print right half of the row
+            for (int j = 1; j  <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = n; i  >= 1; i--) {
+            // Print left half of the row
+            for (int j = 1; j  <= i; j++) {
+                System.out.print("*");
+            }
+            // Print spaces in the middle
+            for (int j = 1; j  <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Print right half of the row
+            for (int j = 1; j  <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 }
 
