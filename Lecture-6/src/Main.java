36bcc32d2582a0a9824142a93736f9573fa1923e
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-02-28 18:19:58 -0500

    Lec-6

--- Lecture-6/src/Main.java
+++ Lecture-6/src/Main.java
@@ -9,28 +9,35 @@
 //    }
 //}
 
-//public class Main implements Runnable {
-//    public static void main(String[] args) {
-//        Main main = new Main();
-//        Thread thread = new Thread(main);
-//        thread.start();
-//        System.out.println("Hello, World!");
-//    }
+public class Main implements Runnable {
+    private static int account = 0;
+    public static void main(String[] args) {
+        Main main = new Main();
+        Thread thread = new Thread(main);
+        thread.start();
+        while (thread.isAlive()) {
+            System.out.println("Waiting++");
+        }
+
+        account++;
+        System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void run() {
+        account++;
+        System.out.println("This is from an thread.");
+    }
+
+}
+//import thread.*;
 //
-//    @Override
-//    public void run() {
-//        System.out.println("This is from an thread.");
-//    }
+//public class Main {
+//    public static void main(String[] args) {
+//        ThreadExample threadExample = new ThreadExample(5);
+//        threadExample.start();
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("This is from Main: " + i);
+//        }
 //    }
 //}
-import thread.*;
-
-public class Main {
-    public static void main(String[] args) {
-        ThreadExample threadExample = new ThreadExample(5);
-        threadExample.start();
-        for (int i = 0; i < 5; i++) {
-            System.out.println("This is from Main: " + i);
-        }
-    }
-}
