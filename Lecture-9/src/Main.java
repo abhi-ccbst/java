54855961d724baacee26b4e87d38482efcfc153c
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-03-10 18:09:31 -0400

    Lec-9

--- Lecture-9/src/Main.java
+++ Lecture-9/src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+
+    private final static ThreadLocal<Integer> threadLocal = ThreadLocal.withInitial(() -> 0);
     public static void main(String[] args) {
         SynchronizedMethods synchronizedMethods = new SynchronizedMethods();
         Thread t1 = new Worker(synchronizedMethods);
@@ -13,5 +15,23 @@ public class Main {
             System.out.println(e.getMessage());
         }
         System.out.println("Final count: " + synchronizedMethods.getSum());
+
+        Runnable task = () -> {
+            for (int i = 0; i < 3; i++) {
+                int value = threadLocal.get();
+                threadLocal.set(value+1);
+                System.out.println(Thread.currentThread().getName() + " -> " + threadLocal.get());
+            }
+        };
+        Thread task1 = new Thread(task, "Task-1");
+        Thread task2 = new Thread(task, "Task-2");
+        task1.start();
+        task2.start();
+        try {
+            task1.join();
+            task2.join();
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
