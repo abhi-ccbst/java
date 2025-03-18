33ba6c734e4a1741190bbbf08175f33bdc815be4
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-03-18 18:53:20 -0400

    Lec-11

--- Lecture-11/src/Main.java
+++ Lecture-11/src/Main.java
@@ -5,57 +5,68 @@ import java.awt.event.ActionEvent;
 public class Main {
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Swing App");
-        jFrame.setSize(400, 300);
+        jFrame.setSize(300, 200);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JTextField jTextField = new JTextField(20);
 //        JLabel label = new JLabel("Hello, Swing!");
 //        jFrame.add(label);
-
-
-        jFrame.setLayout(new BorderLayout());
-        JButton north = new JButton("NORTH");
-        north.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Button Clicked! (NORTH)");
-            }
-        });
-        jFrame.add(north, BorderLayout.NORTH);
-
-        JButton south = new JButton("SOUTH");
+        JButton south = new JButton("Submit");
         south.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Button Clicked! (SOUTH)");
+                System.out.println("Entered texts: " + jTextField.getText());
             }
         });
-        jFrame.add(south, BorderLayout.SOUTH);
+        JPanel panel = new JPanel();
+        panel.add(jTextField);
+        panel.add(south);
+        jFrame.add(panel);
 
-        JButton east = new JButton("EAST");
-        east.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Button Clicked! (EAST)");
-            }
-        });
-        jFrame.add(east, BorderLayout.EAST);
-
-        JButton west = new JButton("WEST");
-        west.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Button Clicked! (WEST)");
-            }
-        });
-        jFrame.add(west, BorderLayout.WEST);
-
-        JButton center = new JButton("CENTER");
-        center.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Button Clicked! (CENTER)");
-            }
-        });
-        jFrame.add(center, BorderLayout.CENTER);
+//        jFrame.setLayout(new BorderLayout());
+//        JButton north = new JButton("NORTH");
+//        north.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.out.println("Button Clicked! (NORTH)");
+//            }
+//        });
+//        jFrame.add(north, BorderLayout.NORTH);
+//
+//        JButton south = new JButton("SOUTH");
+//        south.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.out.println("Button Clicked! (SOUTH)");
+//            }
+//        });
+//        jFrame.add(south, BorderLayout.SOUTH);
+//
+//        JButton east = new JButton("EAST");
+//        east.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.out.println("Button Clicked! (EAST)");
+//            }
+//        });
+//        jFrame.add(east, BorderLayout.EAST);
+//
+//        JButton west = new JButton("WEST");
+//        west.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.out.println("Button Clicked! (WEST)");
+//            }
+//        });
+//        jFrame.add(west, BorderLayout.WEST);
+//
+//        JButton center = new JButton("CENTER");
+//        center.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.out.println("Button Clicked! (CENTER)");
+//            }
+//        });
+//        jFrame.add(center, BorderLayout.CENTER);
         jFrame.setVisible(true);
     }
 }
