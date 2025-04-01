1a810e08c10a196e1eac1cce3e7c7fad9708afbd
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-04-01 18:39:29 -0400

    Lec-2

--- Advance Andriod/Lecture-2/app/src/main/java/com/example/lecture_2/MainActivity.java
+++ Advance Andriod/Lecture-2/app/src/main/java/com/example/lecture_2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.lecture_2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
     String msg = "Android : ";
@@ -13,6 +15,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(msg, "The onCreate() event");
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
     }
 
     /** Called when the activity is about to become visible. */
